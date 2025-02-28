9efe84ac12178bdd4c3e53aae857023fbf1d0105
SHRUTI CHAKRABORTY <90564186+chakraborty-shruti@users.noreply.github.com>
2025-02-28 12:44:14 +0530

    InorderTraversal.java

---
+++ InorderTraversal.java
@@ -0,0 +1,43 @@
+package Tree;
+
+class Node{
+    int key;
+    Node left;
+    Node right;
+
+    Node(int k){
+        key = k;
+        left = null;
+        right = null;
+    }
+}
+
+public class InorderTraversal {
+
+    static void preorderTraversal(Node node){
+        //Base Case
+        if(node == null){
+            return;
+        }
+
+        //Recur on the Left Tree
+        preorderTraversal(node.left);
+
+        //Visit the current node
+        System.out.println(node.key+" ");
+
+        //Recur on the Right Tree
+        preorderTraversal(node.right);
+
+    }
+    
+    public static void main(String[] args){
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.right.left = new Node(40);
+        root.right.right = new Node(50);
+
+        preorderTraversal(root);
+    }
+}
