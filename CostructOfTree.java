93cdd53cb7ac558693cc81a92627de73d2ef067c
SHRUTI CHAKRABORTY <90564186+chakraborty-shruti@users.noreply.github.com>
2025-02-28 11:15:51 +0530

    ConstructionOfTree.java

---
+++ CostructOfTree.java
@@ -0,0 +1,22 @@
+//Simplest way of construction of a tree
+
+class Node{
+    int key;
+    Node left;
+    Node right;
+
+    Node(int k){
+        key = k;
+        left = null;
+        right = null;
+    }
+}
+
+class CostructOfTree {
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.left.left = new Node(40);
+    }
+}
